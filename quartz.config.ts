527de1732916b9737064719928e688bc0dded591
Mononen <alex@mononen.dev>
2026-04-16 12:09:02 -0400

    Quartz sync: Apr 16, 2026, 12:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Guide",
+    pageTitle: "MonoMotion Docs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
